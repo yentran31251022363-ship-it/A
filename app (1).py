4a35bdaeaad064c6ce42df1399755a6cc5715f6b
yentran31251022363-ship-it <yentran.31251022363@st.ueh.edu.vn>
2026-06-21 05:49:17 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -299,7 +299,7 @@ with tabs[1]:
             # KHỐI TỔNG BILL TỰ ĐỘNG XÓA BỎ Ô ĐEN
             st.markdown(f"""
             <div style='background-color: #FFFDF6; border: 1px solid #EAE0C5; border-radius: 16px; padding: 20px; display: flex; justify-content: space-between; align-items: center; margin-top: 25px; margin-bottom: 25px;'>
-                <span style='font-size: 1.1rem; font-weight: bold; color: #5D5446 !important;'>TỔNG CỘNG:</span>
+                <span style='font-size: 1.1rem; font-weight: bold; color: #000000 !important;'>TỔNG CỘNG:</span>
                 <span style='font-size: 2.3rem; font-weight: 800; color: #435241 !important;'>{total_bill:,}đ</span>
             </div>
             """, unsafe_allow_html=True)
